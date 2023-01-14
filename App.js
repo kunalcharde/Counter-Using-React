a0c62986ae69799aeb2271555778c2ec00480d4d
Kunal Charde <kunalcharde1@gmail.com>
2023-01-15 00:43:37 +0530

    resolved

---
+++ App.js
@@ -0,0 +1,12 @@
+import Counter from "./CounterComponent()";
+import "../index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
